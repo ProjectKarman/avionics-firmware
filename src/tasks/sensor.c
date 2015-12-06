e16867135d75060fdbff949d610f8660ae0522fb
tym0027 <tarupprecht@gmail.com>
2015-12-05 20:09:05 -0500

    Task now operates more as a state machine dependent on the entries in a queue
    The queue entries signify when the task should poll certain devices.
    
    This controls the frequency we poll at.

--- src/tasks/sensor.c
+++ src/tasks/sensor.c
@@ -10,6 +10,7 @@
 #include "asf.h"
 #include "FreeRTOS.h"
 #include "task.h"
+#include "queue.h"
 
 #include "ms5607_02ba.h"
 // #include "fxls8471qr1.h"
@@ -19,35 +20,67 @@
 #include "message_types.h"
 #include "transceiver.h"
 
+/* Constant Variables */
+#define SESNORCOLLECTION_100Hz TCC2
+#define SESNORCOLLECTION_400Hz TCD2
+#define SESNORCOLLECTION_800Hz TCE2
+#define EVENT_DATA_SIZE_MAX 25
+#define EVENT_QUEUE_DEPTH 8
+
+enum sensor_queue_state_type {
+	SENSOR_ENTRY_100Hz,
+	SENSOR_ENTRY_400Hz,
+	SENSOR_ENTRY_800Hz
+};
+
+typedef struct {
+	enum sensor_queue_state_type type;
+	uint8_t data[EVENT_DATA_SIZE_MAX];
+} sensor_timer_t;
+
+static QueueHandle_t sensor_queue;
+
 /* Private Prototypes */
 static void sensor_task_loop();
-static void sensor_read();
 static void sensor_initialize();
 static void startup_timer();
 static void send_to_tranceiver();
+static void protocol_timer1_overflow_handler();
+static void protocol_timer2_overflow_handler();
+static void protocol_timer3_overflow_handler();
+static inline void add_priority_event(enum sensor_queue_state_type event_type, void *data);
 
-/* Private Variables */
-#define SENSOR_COLLECTION_TIMER	TCC2
 TaskHandle_t sensor_task_handle;
-uint8_t sensor_read_cycle_counter=0;
+static QueueHandle_t sensor_queue;
 sensors_message_t current_sensor_readings;
-  
+
 void sensor_start_task(void) {
   xTaskCreate(sensor_task_loop, "sensor", 1024, NULL, 2, &sensor_task_handle);
+  sensor_queue = xQueueCreate(EVENT_QUEUE_DEPTH, sizeof(sensor_timer_t));
 }
-
 
 /* Private Functions */
 static void sensor_task_loop() {
   TaskHandle_t sensor_task_handle;
-
+  sensor_timer_t timer_update;
+  
   memset(&current_sensor_readings, 0, sizeof(sensors_message_t));
   
   sensor_initialize();
   startup_timer();
   
-  sensor_read();
   for(;;) {
+    xQueueReceive(sensor_queue, &timer_update, portMAX_DELAY);
+	  
+    switch(timer_update.type) {
+      case SENSOR_ENTRY_800Hz:
+        break;
+	  case SENSOR_ENTRY_400Hz:
+	    break;	
+      case SENSOR_ENTRY_100Hz:
+        break;
+    }
+	send_to_tranceiver();
   }
 }  
 
@@ -61,38 +94,32 @@ static void sensor_initialize() {
   nrf24l01p_init();
 }
 
-static void sensor_read() {
-  uint8_t bool_reset_counter;
-  
-  if (sensor_read_cycle_counter = 0) {
-	// read sensor at 400 Hz
-	  
-	// read sensor at 100 Hz
-	  
-  }
-  else if((sensor_read_cycle_counter % 2) ==  1) {
-    // read sensor at 400 Hz
-  }
-  
-  // read sensor at 800 Hz
-  
-  if (sensor_read_cycle_counter==7) {
-    sensor_read_cycle_counter = 0;
-  }
-  
-  send_to_tranceiver(current_sensor_readings);
-  startup_timer()
-}
-
 static void startup_timer()  {
-  // set up timer
-  tc_enable(&SENSOR_COLLECTION_TIMER);
-  tc_set_overflow_interrupt_callback(&SENSOR_COLLECTION_TIMER, sensor_read);
-  tc_set_wgm(&SENSOR_COLLECTION_TIMER, TC_WG_NORMAL);
-  tc_write_period(&SENSOR_COLLECTION_TIMER, 1.25);
-  tc_set_overflow_interrupt_level(&SENSOR_COLLECTION_TIMER, TC_INT_LVL_LO);
-  cpu_irq_enable();
-  tc_write_clock_source(&SENSOR_COLLECTION_TIMER, TC_CLKSEL_DIV1_gc);
+  // Configure timer to generate frame phase interrupts
+  tc_enable(&SESNORCOLLECTION_100Hz);
+  tc_enable(&SESNORCOLLECTION_400Hz);
+  tc_enable(&SESNORCOLLECTION_800Hz);
+  
+  tc_set_wgm(&SESNORCOLLECTION_100Hz, TC_WG_NORMAL);
+  tc_set_wgm(&SESNORCOLLECTION_400Hz, TC_WG_NORMAL);
+  tc_set_wgm(&SESNORCOLLECTION_800Hz, TC_WG_NORMAL);
+  
+  // tc_write_period(&SESNORCOLLECTION_100Hz, F_CPU / (4 * PROTOCOL_FRAME_RATE));
+  // tc_write_period(&SESNORCOLLECTION_400Hz, F_CPU / (4 * PROTOCOL_FRAME_RATE));
+  // tc_write_period(&SESNORCOLLECTION_800Hz, F_CPU / (4 * PROTOCOL_FRAME_RATE));
+  
+  // Do these channels (second argument) need to be different? Or does each timer have its own TC_CCA?
+  tc_write_cc(&SESNORCOLLECTION_100Hz, TC_CCA, 24000); // TODO: Come up with a general eqn
+  tc_write_cc(&SESNORCOLLECTION_400Hz, TC_CCA, 24000); // TODO: Come up with a general eqn
+  tc_write_cc(&SESNORCOLLECTION_800Hz, TC_CCA, 24000); // TODO: Come up with a general eqn
+  
+  tc_set_overflow_interrupt_callback(&SESNORCOLLECTION_100Hz, protocol_timer1_overflow_handler);
+  tc_set_overflow_interrupt_callback(&SESNORCOLLECTION_400Hz, protocol_timer2_overflow_handler);
+  tc_set_overflow_interrupt_callback(&SESNORCOLLECTION_800Hz, protocol_timer3_overflow_handler);
+  
+  tc_set_overflow_interrupt_level(&SESNORCOLLECTION_100Hz, TC_INT_LVL_MED);
+  tc_set_overflow_interrupt_level(&SESNORCOLLECTION_400Hz, TC_INT_LVL_MED);
+  tc_set_overflow_interrupt_level(&SESNORCOLLECTION_800Hz, TC_INT_LVL_MED);
 }
 
 static void send_to_tranceiver() {
@@ -104,3 +131,23 @@ static void send_to_tranceiver() {
   // transceiver_send_message/transceiver_message_create
   transceiver_send_message(created_message, 0);
 }
+
+static void protocol_timer1_overflow_handler() {
+	add_priority_event(SENSOR_ENTRY_100Hz, NULL);
+}
+
+static void protocol_timer2_overflow_handler() {
+	add_priority_event(SENSOR_ENTRY_400Hz, NULL);
+}
+
+static void protocol_timer3_overflow_handler() {
+	add_priority_event(SENSOR_ENTRY_800Hz, NULL);
+}
+
+static inline void add_priority_event(enum sensor_queue_state_type event_type, void *data) {
+	sensor_timer_t event = {
+		.type = event_type
+	};
+	memcpy(event.data, data, EVENT_DATA_SIZE_MAX);
+	xQueueSendToFrontFromISR(sensor_queue, &event, NULL);
+};
