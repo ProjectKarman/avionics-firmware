bc08b341147dac1663a636c2f7e05abee609f809
Andrew Kaster <andrewdkaster@gmail.com>
2016-02-14 18:35:17 -0500

    added more defines and a few function prototypes

--- src/drivers/hmc5883l.h
+++ src/drivers/hmc5883l.h
@@ -9,6 +9,15 @@
 #ifndef HMC5883L_H_
 #define HMC5883L_H_
 
+//Min and max values stored in data registers
+#define HMC5883L_OUTPUT_MIN		 0xF800
+#define HMC5883L_OUTPUT_MAX		 0x07FF
+
+//In the event of ADC over/underflow or math overflow during bias calc
+//Data register will have the value of -4096 until next valid measurement
+#define HMC5883L_OUTPUT_ERROR	 0xF000 
+
+
 enum hmc5883l_sensor_mode_t{
 	HMC5883L_MODE_CONTINUOUS,
 	HMC5883L_MODE_SINGLE,
@@ -32,5 +41,24 @@ enum hmc5883l_reg_t {
 };
 
 
+typedef struct {
+	int16_t x_angle;
+	int16_t y_angle;
+	int16_t z_angle;
+} hmc5883l_angle_t;
+
+/*Read position data from magnetometer
+	PRECONDITION: measurement mode has been set to single
+	Sends I2C messages to module to read starting at register 3 (XMSB)
+	Stores the result in a angle_t struct passed as a parameter
+*/
+void hmc5883l_read_data_single (hmc5883l_angle_t *angle_ptr);
+
+/*Read position data from magnetometer
+	PRECONDITION: measurement mode has been set to continuous
+	Sends I2C messages to module to read starting at register 3 (XMSB)
+	Stores the result in a angle_t struct passed as a parameter
+*/
+void hmc5883l_read_data_continuous (hmc5883l_angle_t *angle_ptr);
 
 #endif /* HMC5883L_H_ */
