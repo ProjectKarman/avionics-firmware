41df4a7cdf739055fe11239fc3df9ce5d2a803ac
Andrew Kaster <andrewdkaster@gmail.com>
2016-02-27 19:15:09 -0500

    Added Tim's sensor.c file from 2/27 to my branch

--- src/tasks/sensor.c
+++ src/tasks/sensor.c
@@ -8,6 +8,7 @@
 #include "sensor.h"
 
 #include "asf.h"
+//#include "tc.h"
 #include "FreeRTOS.h"
 #include "task.h"
 #include "queue.h"
@@ -20,9 +21,12 @@
 #include "transceiver.h"
 
 /* Constant Variables */
+#define SENSOR_TIMER TCE2
+
 #define SESNORCOLLECTION_100Hz TCC2
 #define SESNORCOLLECTION_400Hz TCD2
 #define SESNORCOLLECTION_800Hz TCE2
+
 #define EVENT_DATA_SIZE_MAX 25
 #define EVENT_QUEUE_DEPTH 8
 #define BASE_HERTZ_MODIFIER 40000
@@ -48,6 +52,9 @@ static void send_to_tranceiver();
 static void protocol_timer1_overflow_handler();
 static void protocol_timer2_overflow_handler();
 static void protocol_timer3_overflow_handler();
+
+static void protocol_timer_overflow_handler();
+
 static inline void add_priority_event(enum sensor_queue_state_type event_type, void *data);
 
 TaskHandle_t sensor_task_handle;
@@ -63,50 +70,47 @@ void sensor_start_task(void) {
 static void sensor_task_loop() {
   TaskHandle_t sensor_task_handle;
   sensor_timer_t timer_update;
+  static uint8_t temp_debug_variable_breakpoint = 0;
   
   memset(&current_sensor_readings, 0, sizeof(sensors_message_t));
   
   sensor_initialize();
   startup_timer();
-  uint8_t retval = 0;
-  rocket_press_t press;
-  rocket_temp_t temp;
-  uint32_t d1 = 0;
-  uint32_t d2 = 0;
+  ms5607_02ba_reset();
+  ms5607_02ba_load_prom();
+  
+  temp_debug_variable_breakpoint = 10;
+  
+  
   for(;;) {
     xQueueReceive(sensor_queue, &timer_update, portMAX_DELAY);
-	  
+	temp_debug_variable_breakpoint += 1; 
     switch(timer_update.type) {
       case SENSOR_ENTRY_800Hz:
+		temp_debug_variable_breakpoint += 1;
         break;
 	  case SENSOR_ENTRY_400Hz:
-		 ms5607_02ba_convert_d1(OSR_4096);
-		 vTaskDelay(9);
-		 ms5607_02ba_read_adc(&d1);
-		ms5607_02ba_convert_d2(OSR_4096);
-		vTaskDelay(9);
-		ms5607_02ba_read_adc(&d2);
-		temp = ms5607_02ba_calculate_temp(d2);
-
-		press = ms5607_02ba_calculate_press(d1, d2);
+		temp_debug_variable_breakpoint += 2;
 	    break;	
       case SENSOR_ENTRY_100Hz:
+		temp_debug_variable_breakpoint += 3;
         break;
     }
-	send_to_tranceiver();
+	// send_to_tranceiver();
+	temp_debug_variable_breakpoint += 1;
   }
 }  
 
 static void sensor_initialize() {
-  ms5607_02ba_reset();
-  ms5607_02ba_load_prom();
+  ms5607_02ba_init();
   
   // fxls8471qr1_init(void);
   // l3g4200d_init();
   
-  nrf24l01p_init();
+  // nrf24l01p_init();
 }
 
+/*
 static void startup_timer()  {
   // Configure timer to generate frame phase interrupts
   tc_enable(&SESNORCOLLECTION_100Hz);
@@ -128,6 +132,30 @@ static void startup_timer()  {
   tc_set_overflow_interrupt_level(&SESNORCOLLECTION_100Hz, TC_INT_LVL_MED);
   tc_set_overflow_interrupt_level(&SESNORCOLLECTION_400Hz, TC_INT_LVL_MED);
   tc_set_overflow_interrupt_level(&SESNORCOLLECTION_800Hz, TC_INT_LVL_MED);
+}
+*/
+
+static void startup_timer()
+{
+	/*
+	 SENSOR_TIMER.CNT = 0;// Zeroise count
+	 SENSOR_TIMER.PER = 800; //Period
+	 SENSOR_TIMER.CTRLA = TC_CLKSEL_DIV1_gc; //Divider
+	 SENSOR_TIMER.INTCTRLA = TC_OVFINTLVL_LO_gc; //Liow level interrupt
+	 SENSOR_TIMER.INTFLAGS = 0x01; // clear any initial interrupt flags
+	 SENSOR_TIMER.CTRLB = TC_WGMODE_NORMAL_gc; // Normal operation
+	 
+	 PMIC.CTRL |= PMIC_LOLVEN_bm;
+	 */
+	
+	tc_enable(&SENSOR_TIMER);
+	tc_set_wgm(&SENSOR_TIMER, TC_WG_NORMAL);
+	tc_write_period(&SENSOR_TIMER, F_CPU / (8 * BASE_HERTZ_MODIFIER));
+	
+	tc_set_overflow_interrupt_callback(&SENSOR_TIMER, protocol_timer_overflow_handler);
+	tc_set_overflow_interrupt_level(&SENSOR_TIMER, TC_INT_LVL_LO);
+	
+	tc_write_clock_source(&SENSOR_TIMER, TC_CLKSEL_DIV1_gc);
 }
 
 static void send_to_tranceiver() {
@@ -152,10 +180,32 @@ static void protocol_timer3_overflow_handler() {
 	add_priority_event(SENSOR_ENTRY_800Hz, NULL);
 }
 
+static void protocol_timer_overflow_handler() {
+	static uint8_t hertz_state = 0;
+	
+	if (hertz_state == 0) {
+		add_priority_event(SENSOR_ENTRY_100Hz, NULL);
+		add_priority_event(SENSOR_ENTRY_400Hz, NULL);
+	}
+	else if ((hertz_state & 1) == 0) {
+		add_priority_event(SENSOR_ENTRY_400Hz, NULL);
+	}
+	add_priority_event(SENSOR_ENTRY_800Hz, NULL);
+	
+	if (hertz_state == 7)
+	{
+		hertz_state = 0;
+	}
+	else{
+		hertz_state+=1;
+	}
+}
+
 static inline void add_priority_event(enum sensor_queue_state_type event_type, void *data) {
 	sensor_timer_t event = {
 		.type = event_type
 	};
 	memcpy(event.data, data, EVENT_DATA_SIZE_MAX);
-	xQueueSendToFrontFromISR(sensor_queue, &event, NULL);
+	// xQueueSendToFrontFromISR(sensor_queue, &event, NULL);
+	xQueueSendToFront(sensor_queue, &event, NULL);
 };
